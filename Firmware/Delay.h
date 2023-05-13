e0162af5aff4f7f4eaaaee690a81ea98c1c6b053
Jonathan Brodsky <jonbro@gmail.com>
2023-05-13 15:37:03 -0700

    delay unit supports different speeds

--- Firmware/Delay.h
+++ Firmware/Delay.h
@@ -3,13 +3,14 @@
 #include "pico/stdlib.h"
 
 using namespace braids;
-#define DELAY_LENGTH 10000
+#define DELAY_LENGTH 20000
 class Delay2 {
  public:
     Delay2()
     {
         feedback = 0;
         feedbackAmount = 0x4fff;
+        phaseOffset = 0x41893;
     }
     inline void process(int16_t in, int16_t& l, int16_t& r)
     {
@@ -19,28 +20,31 @@ class Delay2 {
  private:
     int16_t DelayProcess(int16_t in)
     {
-        int16_t delayed = buf[count];
-        buf[count] = in;
-        if (++count > length-1)
-            count = 0;
-        return delayed;
-    }
-    int16_t DelayTap(int tap)
-    {
-        int offset = tap+count;
-        while (offset < 0)
-            offset += length;
-        while(offset > length-1)
+        uint64_t offsetFull = ((uint64_t)length*(uint64_t)(phase)) >> 16;
+        int32_t offset = offsetFull >> 16;
+        int16_t a = buf[offset];
+        int16_t b = buf[(offset-1)<0?length+(offset-1):offset-1];
+        int16_t delayed =  Mix(b, a, offsetFull&0xffff);
+
+        int32_t writePos = offset-2;
+        if(writePos < 0)
+            writePos = length+writePos;
+        // fill in the extra samples in case we are writing faster than normal speed 
+        while((lastWritePosition+1)%length != writePos)
         {
-            offset -= length;
+            lastWritePosition = (lastWritePosition+1)%length;
+            buf[lastWritePosition] = in;
         }
-        return buf[offset];
+        phase += phaseOffset;
+        return delayed;
     }
 
     q15_t  feedbackAmount;
     int16_t feedback;
     int length = DELAY_LENGTH;
     int count = 0;
-
+    uint32_t lastWritePosition = 0;
+    uint32_t phase = 0;
+    uint32_t phaseOffset = 0x10624; // how far we step
     int16_t buf[DELAY_LENGTH] = {0};
 };
